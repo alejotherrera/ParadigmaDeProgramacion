90f2fedade79bf056813542c8bda0884501962f2
alejotherrera <Alejotomasherrera@hotmail.com>
2021-10-23 18:17:46 -0300

    commitnuevo2

--- commitgit.sh
+++ commitgit.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 echo "Bienvenido a la gestion de commit"
-read ruta
+
 echo "Pulse una tecla para git status"
 read tecla
-git status 
+git status
+
 echo "Pulse una tecla para git add ."
 read tecla
 git add .
+
 echo "Pulse una telca para git commit -m"
 read tecla
 echo "Ingrese el nombre del commit: "
 read commitname
 git commit -m "$commitname"
 echo "Commit preparado"
+
 echo "Ingrese una tecla para git status"
 read tecla
 git status
+
 echo "Ingrese una tecla para git log"
 read tecla
+
 echo "Ingrese la rama a la que desea actualizar"
 read ramabranch
-git push origin $ramabranch
+git push --set-upstream origin $ramabranch
 
 echo "Commit finalizado"
