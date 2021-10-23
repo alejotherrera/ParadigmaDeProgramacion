81a6d0b6f06a53db8dd722b95f12622cbb6fa628
alejotherrera <Alejotomasherrera@hotmail.com>
2021-10-23 18:31:28 -0300

    pruebacommit

--- commitgit.sh
+++ commitgit.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
-echo "Prueba"
-echo "Bienvenido a la gestion de commit--betaS"
+
+echo "Bienvenido a la gestion de commit--beta"
 
 echo "Pulse una tecla para git status"
 read tecla
 git status
-
+echo "-----------------------------------"
 echo "Pulse una tecla para git add ."
 read tecla
 git add .
-
+echo "-----------------------------------"
 echo "Pulse una telca para git commit -m"
 read tecla
 echo "Ingrese el nombre del commit: "
 read commitname
 git commit -m "$commitname"
 echo "Commit preparado"
-
+echo "-----------------------------------"
 echo "Ingrese una tecla para git status"
 read tecla
 git status
-
-
-echo "Desea continuar?"
-git push --set-upstream origin main
-
-echo "Commit finalizado"
+echo "-----------------------------------"
+echo "Ingrese la rama que desea modificar: (enter para default)"
+read ramabranch
+git push origin $ramabranch
+echo "-----------------------------------"
+echo "Commit finalizado, gracias por utilizar el programa"
+echo " Alejo Herrera- DEVscript"
