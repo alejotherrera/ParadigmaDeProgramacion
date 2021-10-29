f6cfde5f73e7d6754c2a31b2b9050c9fd84eb740
alejotherrera <Alejotomasherrera@hotmail.com>
2021-10-29 17:05:03 -0300

    Adding Prolog files

---
+++ Prolog/TrabajoPractico3_Herrera.pl
@@ -0,0 +1,25 @@
+variacionRansomware(1, Grandcrab, 78.5, 10/01/2020, [enero,febrero,marzo,abril]).
+variacionRansomware(2, Babuk, 7.61, 05/07/2020, [julio,agosto]).
+variacionRansomware(3, Cerber, 3.11, 20/01/2020, [enero,febrero,diciembre]).
+variacionRansomware(4, Matsnu, 2.63, 03/04/2020, [abril,octubre]).
+variacionRansomware(5, Wannacry, 2.41, 29/06/2020, [junio,septiembre]).
+variacionRansomware(6, Congur, 1.52, 05/07/2020, [julio]).
+variacionRansomware(7, Locky, 1.29, 12/05/2020, [mayo,diciembre]).
+variacionRansomware(8, Teslacrypt, 1.12, 04/11/2020, [noviembre,diciembre]).
+variacionRansomware(9, Rkor, 1.11, 10/03/2020, [marzo,mayo]).
+variacionRansomware(10, Reveton, 0.70, 29/08/2020, [agosto]).
+
+distribucion(1, Pe).
+distribucion(2, Android).
+distribucion(3, Dll).
+distribucion(4, Gzip).
+distribucion(5, Javascript).
+
+troyano(Emotet, True, 1, [2,3,4]).
+troyano(Ezbot, False, 3, [6,1,10,2]).
+troyano(Edridex, False, 5, [9,5,3]).
+troyano(Egozi, True, 4, [7,10,4]).
+troyano(Edanabot, True, 2, [8,1,5]).
+
+
+
